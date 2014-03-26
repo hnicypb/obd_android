1db4254972eac896ebe3c0cc742a7e0fbe000650
hnicypb <hnicypb@gmail.com>
2014-03-26 14:55:14 +0800

    第一次提交
    Signed-off-by: hnicypb <hnicypb@gmail.com>

---
+++ src/com/vchecker/obd/main/FragmentPage_detail.java
@@ -0,0 +1,16 @@
+package com.vchecker.obd.main;
+
+import com.vchecker.obd.*;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentPage_detail extends Fragment{
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {	
+		return inflater.inflate(R.layout.fragment_detail, null);		
+	}	
+}
